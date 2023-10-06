8d02f9309864d3f574e770ec8bf51f7276b58977
DanielDavis <danieldavis998@gmail.com>
2023-10-06 17:58:33 +1300

    simplify math list

--- src/math_structures.py
+++ src/math_structures.py
@@ -1,10 +1,12 @@
+from typing import Self
+
 OPERATORS = ['+', '-', '*', '/']
 DEBUG = False
 
 
 class MathList:
 
-    def __init__(self, expression: list[str], result: int):
+    def __init__(self, expression: list[str] = None):
         """
 
         Args:
@@ -13,27 +15,39 @@ class MathList:
             result: The result of the expression
         """
         self.expression = expression
-        self.result = result
 
-    def __add__(self, other):
+        if self.expression is not None:
+            self._validate_expression(expression)
+            self.result = self._evaluate_expression(expression)
+        else:
+            self.result = None
+
+    @classmethod
+    def _from_math_list(cls, expression, result):
+        inst = MathList()
+        inst.expression = expression
+        inst.result = result
+        return inst
+
+    def __add__(self, other: Self) -> Self:
         new_expression = ['+'] + self.expression + other.expression
         new_result = self.result + other.result
 
-        return MathList(new_expression, new_result)
+        return MathList._from_math_list(new_expression, new_result)
 
-    def __sub__(self, other):
+    def __sub__(self, other: Self) -> Self:
         new_expression = ['-'] + self.expression + other.expression
         new_result = self.result - other.result
 
-        return MathList(new_expression, new_result)
+        return MathList._from_math_list(new_expression, new_result)
 
-    def __mul__(self, other):
+    def __mul__(self, other: Self) -> Self:
         new_expression = ['*'] + self.expression + other.expression
         new_result = self.result * other.result
 
-        return MathList(new_expression, new_result)
+        return MathList._from_math_list(new_expression, new_result)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: Self) -> Self:
         if other.result == 0:
             raise ZeroDivisionError
         if self.result % other.result != 0:
@@ -42,112 +56,76 @@ class MathList:
         new_expression = ['/'] + self.expression + other.expression
         new_result = int(self.result / other.result)
 
-        return MathList(new_expression, new_result)
+        return MathList._from_math_list(new_expression, new_result)
 
-    def __eq__(self, other):
-        return self.result == other.result
+    def __eq__(self, other) -> bool:
+        if isinstance(other, int):
+            return self.result == other
+        if isinstance(other, float):
+            return self.result == other
+        elif isinstance(other, MathList):
+            return self.result == other.result
+        raise NotImplementedError(f"Equality not implemented for MathList and {type(other)}")
 
     def __hash__(self):
         return tuple(self.expression).__hash__()
 
-    def sort_atom(self):
-        pass
+    def __str__(self):
+        return f"{self.to_infix()}={self.result}"
 
-    def verify_atom(self):
-        pass
+    def __repr__(self):
+        return self.expression, self.result
 
-    def evaluate_atom(self):
+    @staticmethod
+    def _validate_expression(expression) -> None:
+        num_operators = sum(item in OPERATORS for item in expression)
+        num_digits = sum(item.isdigit() for item in expression)
+        if num_operators != num_digits - 1:
+            raise ValueError(f"Expression '{expression}' does not have the correct ratio of numbers and operators.")
+
+    @classmethod
+    def break_down_expression(cls, expression):
+        operator = expression[0]
+        assert operator in OPERATORS, f"{operator} must be in {OPERATORS}"
+
+        index, count = 1, 0
+        while True:
+            if expression[index] in OPERATORS:
+                count += 1
+            elif expression[index].isdigit():
+                count -= 1
+
+            index += 1
+            if count < 0:
+                break
+
+        return operator, expression[1:index], expression[index:]
+
+    @classmethod
+    def _evaluate_expression(cls, expression) -> int:
+        if len(expression) == 1:
+            return int(expression[0])
+
+        operator, sub_expression_a, sub_expression_b = cls.break_down_expression(expression)
+
+        return eval(
+            f"{cls._evaluate_expression(sub_expression_a)}{operator}{cls._evaluate_expression(sub_expression_b)}")
+
+    def sort(self) -> None:
         pass
 
     def iterate_bottom_up(self, func):
         pass
 
-    def iterate_top_down(self, func):
-        raise NotImplementedError
-        # tmp1, tmp2 = 0, 0
-        # while tmp2 < 99:  # Just in case, should be impossible
-        #     if sub_expression[tmp2] in OPERATORS:
-        #         tmp1 += 1
-        #     elif sub_expression[tmp2].isdigit():
-        #         tmp1 -= 1
-        #     if tmp1 <= 0:
-        #         break
-        #     tmp2 += 1
-        #
-        # index = tmp2 + 1
+    @classmethod
+    def _to_infix_list(cls, expression) -> list[str]:
+        if len(expression) == 1:
+            return expression
 
-    def recurse_bottom_up(self, func):
-        pass
+        operator, sub_expression_a, sub_expression_b = cls.break_down_expression(expression)
 
-    def recurse_top_down(self, func, sub_expression=None, i=0):
-        """Recursively breaks down a prefix expression in more basic expressions
-           to be evaluated.
-        """
-        if sub_expression is None:
-            sub_expression = self.expression
-        # Return if all that is left is a single number
-        if len(sub_expression) == 1:
-            func(self, i)
-        else:
-            # Find index of point between two operands in expression
-            tmp1, tmp2 = 0, 0
-            while tmp2 < 99:  # Just in case, should be impossible
-                if sub_expression[tmp2] in OPERATORS:
-                    tmp1 += 1
-                elif sub_expression[tmp2].isdigit():
-                    tmp1 -= 1
-                if tmp1 <= 0:
-                    break
-                tmp2 += 1
+        return ['('] + cls._to_infix_list(sub_expression_a) + [' ', operator, ' '] + cls._to_infix_list(
+            sub_expression_b) + [')']
 
-            index = tmp2 + 1
-
-            # Recurse after dividing into operator, operand a and operand b
-            operand_a = self.recurse_top_down(func, sub_expression[1:index], 1)
-            operand_b = self.recurse_top_down(func, sub_expression[index:], index)
-
-            func(self, i)
-
-            # Check if an input was erroneus
-            if operand_a == '' or operand_b == '':
-                return ''
-
-            # return func([sub_expression[0],
-            # operand_a,
-            # operand_b])
-
-    def to_infix(self):
-
-        def recurse(sub_expression=None):
-            if sub_expression is None:
-                sub_expression = list(self)
-            # Return if all that is left is a single number
-            if len(sub_expression) == 1:
-                return sub_expression
-            else:
-                # Find index of point between two operands in expression
-                tmp1, tmp2 = 0, 0
-                while tmp2 < 99:  # Just in case, should be impossible
-                    if sub_expression[tmp2] in OPERATORS:
-                        tmp1 += 1
-                    elif sub_expression[tmp2].isdigit():
-                        tmp1 -= 1
-                    if tmp1 <= 0:
-                        break
-                    tmp2 += 1
-
-                index = tmp2 + 1
-
-                # Recurse after dividing into operator, operand a and operand b
-                operand_a = recurse(sub_expression[1:index])
-                operand_b = recurse(sub_expression[index:])
-
-                # Check if an input was erroneous
-                if operand_a == '' or operand_b == '':
-                    return ''
-
-                return ['('] + operand_a + [' ', sub_expression[0], ' '] + operand_b + [')']
-
-        expression = ''.join(recurse())
-
-        return expression[1:-1] if len(self.expression) > 1 else expression
+    def to_infix(self) -> str:
+        return ''.join(self._to_infix_list(self.expression))[1:-1]
--- tests/test_math_structures.py
+++ tests/test_math_structures.py
@@ -8,8 +8,16 @@ class TestMathList(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        self.expression_one = MathList(['+', '2', '1'], 3)
-        self.expression_two = MathList(['-', '4', '3'], 1)
+        self.expression_one = MathList(['+', '2', '1'])
+        self.expression_two = MathList(['-', '4', '3'])
+
+    def test_validation_expression(self):
+        self.assertRaises(ValueError, lambda: MathList._validate_expression(['+', '1']))
+        self.assertRaises(ValueError, lambda: MathList._validate_expression(['+', '1', '1', '1']))
+
+    def test_evaluate_expression(self):
+        self.assertEqual(MathList._evaluate_expression(['+', '2', '1']), 3)
+        self.assertEqual(MathList._evaluate_expression(['-', '4', '3']), 1)
 
     def test_addition(self):
         self.assertEqual(self.expression_one + self.expression_two,
@@ -28,5 +36,5 @@ class TestMathList(unittest.TestCase):
                          self.expression_one.result / self.expression_two.result)
 
     def test_infix_conversion(self):
-        self.assertEqual(self.expression_one.to_infix(), '4 - 3')
-        self.assertEqual((self.expression_one.result + self.expression_two.result).to_infix(), '(2 + 1) + (4 - 3)')
+        self.assertEqual(self.expression_one.to_infix(), '2 + 1')
+        self.assertEqual((self.expression_one + self.expression_two).to_infix(), '(2 + 1) + (4 - 3)')
